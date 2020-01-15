bab0468177650dd77904750c7bd062e5b2848bc0
Paul <paul@wizmacau.com>
2020-01-15 14:59:47 +0800

    update: signal

--- signal/signal.go
+++ signal/signal.go
@@ -7,13 +7,14 @@ package signal
 import (
 	"context"
 	"os"
-	"os/signal"
+	gosignal "os/signal"
+	"sync/atomic"
 	"syscall"
 )
 
 func SigTermCancelContext(ctx context.Context) context.Context {
 	term := make(chan os.Signal)
-	signal.Notify(term, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	gosignal.Notify(term, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
 	c, cancel := context.WithCancel(ctx)
 
@@ -26,4 +27,36 @@ func SigTermCancelContext(ctx context.Context) context.Context {
 	}()
 
 	return c
+}
+
+func Trap(cleanup func(), logger interface {
+	Info(args ...interface{})
+}) {
+	c := make(chan os.Signal, 1)
+	go func() {
+		interruptCount := uint32(0)
+		for sig := range c {
+			if sig == syscall.SIGPIPE {
+				continue
+			}
+			go func(sig os.Signal) {
+				switch sig {
+				case os.Interrupt, syscall.SIGTERM:
+					if atomic.LoadUint32(&interruptCount) < 3 {
+						if atomic.AddUint32(&interruptCount, 1) == 1 {
+							cleanup()
+							os.Exit(0)
+						} else {
+							return
+						}
+					} else {
+						logger.Info("Forcing shutdown without cleanup; 3 interrupts received")
+					}
+				case syscall.SIGQUIT:
+					logger.Info("Forcing shutdown without cleanup on SIGQUIT")
+				}
+				os.Exit(128 + int(sig.(syscall.Signal)))
+			}(sig)
+		}
+	}()
 }
